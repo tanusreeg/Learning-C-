d851c81adb47bb4e7af3aaf15cd53e815a2c16ac
TANUSREE GHOSH <tanusreeghosh857@gmail.com>
2020-12-12 13:44:55 +0530

    Add files via upload

---
+++ Algorithms with C++/Sorting/selection sort.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+// selection sort : finds minimum value from remaining array.. then replaces it with index element..
+// array = 5 4 3 2 1 || At 1st loop it will pick minimum value 1.. and swap with index 0.. || 1 4 3 2 5..
+// in 2nd loop search for minimum will begin from index 1..At 2nd loop it will pick minimum value from remaining :2.. 
+//and swap with index 1.. ||1 2 3 4 5
+
+void swap(int*a, int*b) // simple swap function: takes two pointer as argument
+{
+	int tm;				
+	tm=*a; *a=*b; *b=tm;
+}
+int * find_min(int*arr,int s) // returns pointer to minimum element
+{
+	int min = arr[0];
+	int *min_index_p;
+	for(int i = 1; i<s; i++)
+	{
+		if(arr[i]<min)
+		{
+		min = arr[i];
+		min_index_p=&arr[i];
+		}
+	}
+	return min_index_p;
+}
+void selection_sort(int * array,int s)
+{
+	int * k;
+	for(int i =0; i<s-1; i++)
+	{
+		k = find_min((array+i),s-i);  //k contains pointer to minimum element
+		swap (k,array+i);
+	}
+	
+}
+//driver 
+int main()
+{
+	int *arr= new int[10000];
+	ifstream ifs("RandomNumber_10000.txt");
+	for(int i = 0; i<10000; i++)
+	{
+		ifs>>arr[i];
+	}
+	ifs.close();
+	selection_sort(arr,10000);
+	ofstream ofs("Sorted by selection sort.txt", ios::trunc);
+	for(int i = 0; i<10000; i++)
+	{
+		ofs<<arr[i];
+		ofs<<endl;
+	}
+	cout<<"File created"<<endl;
+}
